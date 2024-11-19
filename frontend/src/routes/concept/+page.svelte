dbd907a809200fdf31087ef857449422edb4bf5d
zilla416 <iicyotb@gmail.com>
2024-11-19 14:06:32 +0100

    responsiveness + nieuwe images

--- frontend/src/routes/concept/+page.svelte
+++ frontend/src/routes/concept/+page.svelte
@@ -1,105 +1,159 @@
 <script>
-  import Header from "../../components/Header.svelte";
-  import Footer from "../../components/Footer.svelte";
+   import Header from "../../components/Header.svelte";
+   import Footer from "../../components/Footer.svelte";
 </script>
-
 <Header />
-
 <main class="pt-[88px] md:pt-28">
-  <section class="mb-16 relative">
-    <div class="relative w-full mx-auto">
-      <img 
-      src="/header.jpeg"
-      alt="Image" 
-      class="w-full h-[40rem] md:h-80 object-cover rounded-b-3xl md:rounded-r-full blur-sm"
-      />
-      <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
-      >
-        <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
-        >
-         <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Het Spiegelconcept</h2>
-         <p class="w-11/12 md:w-3/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
-            Met het Spiegelconcept geeft u invulling aan een lerende organisatie. Met deze aanpak spiegel je je als professional, team en organisatie systematisch aan de hoogste kwaliteitsstandaarden en de behoeften vanuit je omgeving. Je gebruikt de leerpunten die hieruit naar voren komen om je dienstverlening te ontwikkelen.
-        </p>
-                 <p class="w-11/12 md:w-3/4 text-center md:text-left text-lg md:text-base pb-12 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] md:block md:opacity-0 opacity-100">
-            In het Spiegelconcept ligt de focus op teamontwikkeling. Daar vindt de feitelijke dienstverlening immers plaats. Het Spiegelconcept is makkelijk schaalbaar. Daardoor is het concept ook organisatiebreed toepasbaar. 
-        </p>
-   </div>
-   </div>
-   </div>
-  </section>
-
-  <hr class="w-1/4 flex justify-center items-center mx-auto pb-12 md:pb-14" />
-
- <section>
-    <div class="flex flex-col md:flex-row justify-center items-center">
-      <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
-        <h2
-          class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
-        >
-          Het concept
-        </h2>
-        <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
-          Uit ervaring weten we dat de motivatie van in publieke organisaties wel in orde is.  Winst is vooral te behalen als professionals gaan samenwerken aan gedeelde doelen in het team. Daarbij dient het gesprek zich te richten op de prestaties en de kwaliteit van het team. Door de teamleden voortdurend in dialoog te laten gaan, wisselen zij verschillende perspectieven en zienswijzen uit.. Zo leren de teamleden van elkaar en ontstaat ruimte voor vernieuwing. Het team ervaart energie, inspiratie en verbinding. Het team bouwt aan een lerende cultuur.
-        </p>
+   <section class="mb-16 relative">
+      <div class="relative w-full mx-auto">
+         <img 
+            src="/header.jpeg"
+            alt="Headerimg" 
+            class="w-full h-[40rem] md:h-80 object-cover rounded-b-3xl md:rounded-r-full blur-sm"
+            />
+         <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
+            >
+            <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
+               >
+               <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Het Spiegelconcept</h2>
+               <p class="w-11/12 md:w-3/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
+                  Met het Spiegelconcept geeft u invulling aan een lerende organisatie. Met deze aanpak spiegel je je als professional, team en organisatie systematisch aan de hoogste kwaliteitsstandaarden en de behoeften vanuit je omgeving. Je gebruikt de leerpunten die hieruit naar voren komen om je dienstverlening te ontwikkelen.
+               </p>
+               <p class="w-11/12 md:w-3/4 text-center md:text-left text-lg md:text-base pb-12 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] md:block md:opacity-0 opacity-100">
+                  In het Spiegelconcept ligt de focus op teamontwikkeling. Daar vindt de feitelijke dienstverlening immers plaats. Het Spiegelconcept is makkelijk schaalbaar. Daardoor is het concept ook organisatiebreed toepasbaar. 
+               </p>
+            </div>
+         </div>
       </div>
-      <img
-        src="/spiegelconcept.png"
-        alt="Img"
-        class="rounded-3xl w-2/5 md:w-1/4 mt-4 md:mt-0"
-      />
-    </div>
-
-
-    <div
-      class="flex flex-col md:flex-row items-center md:justify-center my-24 md:my-48"
-    >
+   </section>
+   <hr class="w-1/4 flex justify-center items-center mx-auto pb-12 md:pb-14" />
+   <section>
+      <div class="flex flex-col md:flex-row justify-center items-center">
+         <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
+            <h2
+               class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
+               >
+               Het concept
+            </h2>
+            <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
+               Uit ervaring weten we dat de motivatie van in publieke organisaties wel in orde is.  Winst is vooral te behalen als professionals gaan samenwerken aan gedeelde doelen in het team. Daarbij dient het gesprek zich te richten op de prestaties en de kwaliteit van het team. Door de teamleden voortdurend in dialoog te laten gaan, wisselen zij verschillende perspectieven en zienswijzen uit.. Zo leren de teamleden van elkaar en ontstaat ruimte voor vernieuwing. Het team ervaart energie, inspiratie en verbinding. Het team bouwt aan een lerende cultuur.
+            </p>
+         </div>
+         <img
+            src="/spiegelconcept.png"
+            alt="Img"
+            class="rounded-3xl w-2/5 md:w-1/4 mt-10 md:mt-0"
+            />
+      </div>
       <div
-        class=" w-4/5 md:w-1/3 h-48 md:h-72 rounded-3xl md:mr-12"
-      >
+         class="flex flex-col-reverse md:flex-row items-center md:justify-center my-24 md:my-48"
+         >
+         <div
+            class=" w-4/5 md:w-1/3 h-48 md:h-72 rounded-3xl md:mr-12"
+            >
             <img
-        src="/samensturen.jpg"
-        alt="Img"
-        class="w-auto md:w-auto mx-auto my-auto h-48 md:h-72 rounded-3xl md:mr-12"
-      />
+               src="/samensturen.jpg"
+               alt="Img"
+               class="w-auto md:w-auto mt-10 mx-auto my-auto h-48 md:h-72 rounded-3xl md:mr-12"
+               />
+         </div>
+         <div class="text-center md:w-1/3 md:ml-36">
+            <h2 class="text-2xl md:text-3xl font-bold pb-6 md:pt-0">
+               Samen sturen
+            </h2>
+            <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+               Vanuit een positieve insteek willen wij eraan bijdragen dat teams beter functioneren en plezier beleven in hun werk. De omgeving waarin de teams opereren is complex  en clienten en stakeholders zijn kritisch. Van het team vraagt dit daadkracht, professionaliteit en flexibliteit. Op een praktische en speelse manier helpen we teams om deze dynamiek hanteerbaar te maken. Door als team samen sturing te geven aan deze ontwikkelingen kan zij de dienstverlening daarop aanpassen. Wij noemen dat Samen Sturen. Ieder draagt vanuit zijn eigen rol en verantwoordelijkheid bij aan de doelen van het team, of je nu manager, professional of ondersteuner bent. <span>
+            </p>
+         </div>
       </div>
-      <div class="text-center md:w-1/3 md:ml-36">
-        <h2 class="text-2xl md:text-3xl font-bold pb-6 pt-12 md:pt-0">
-          Samen sturen
-        </h2>
-        <p
-          class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
-        >
-          Vanuit een positieve insteek willen wij eraan bijdragen dat teams beter functioneren en plezier beleven in hun werk. De omgeving waarin de teams opereren is complex  en clienten en stakeholders zijn kritisch. Van het team vraagt dit daadkracht, professionaliteit en flexibliteit. Op een praktische en speelse manier helpen we teams om deze dynamiek hanteerbaar te maken. Door als team samen sturing te geven aan deze ontwikkelingen kan zij de dienstverlening daarop aanpassen. Wij noemen dat Samen Sturen. Ieder draagt vanuit zijn eigen rol en verantwoordelijkheid bij aan de doelen van het team, of je nu manager, professional of ondersteuner bent. <span>
-        </p>
+   </section>
+   <section class="mb-16 relative">
+      <div class="relative w-full mx-auto">
+         <img 
+            src="/aanpak.jpg"
+            alt="Aanpakimg" 
+            class="w-full md:w-full h-[20rem] flex justify-center items-center md:h-80 object-cover rounded-b-3xl md:rounded-r-full blur-sm"
+            />
+         <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
+            >
+            <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
+               >
+               <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Aanpak</h2>
+               <p class="w-11/12 md:w-2/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
+                  Het Spiegelconcept bestaat uit verschillende instrumenten, die onderling samenhangen. Hieronder bespreken we kort de belangrijkste instrumenten. 
+               </p>
+            </div>
+         </div>
       </div>
-    </div>
-  </section>
-
-  
-  <section class="mb-16 relative">
-    <div class="relative w-full mx-auto">
-      <img 
-      src="/aanpak.jpg"
-      alt="Image" 
-      class="w-full md:w-full h-[20rem] flex justify-center items-center md:h-80 object-cover rounded-b-3xl md:rounded-r-full blur-sm"
-      />
-      <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
-      >
-        <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
-        >
-         <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Aanpak</h2>
-         <p class="w-11/12 md:w-2/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
-            Het Spiegelconcept bestaat uit verschillende instrumenten, die onderling samenhangen. Hieronder bespreken we kort de belangrijkste instrumenten. 
-        </p>
-   </div>
-   </div>
-   </div>
-  </section>
-
-  <hr class="w-1/4 flex justify-center items-center mx-auto pb-12 md:pb-14" />
-
+   </section>
+   <hr class="w-1/4 flex justify-center items-center mx-auto pb-12 md:pb-14" />
+   <section>
+      <div class="flex flex-col md:flex-row justify-center items-center">
+         <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
+            <h2
+               class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
+               >
+               Het TeamKompas
+            </h2>
+            <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
+               Om de koers van het team te bepalen starten we door samen met het team een TeamKompas te maken. De basis hiervoor zijn de overtuigingen en leidende principes van team. Het Teamkompas geeft het team koers voor de langere termijn. Vervolgens maken we het teamplan dat zo mogelijk gevoed wordt door (zelf-)evaluaties klant- en omgevingsonderzoek.  
+            </p>
+         </div>
+         <img
+            src="/teamkompas.png"
+            alt="Teamkompaspng"
+            class="rounded-3xl w-2/5 md:w-1/4 mt-10 md:mt-0"
+            />
+      </div>
+      <div
+         class="flex flex-col-reverse md:flex-row items-center md:justify-center my-24 md:my-48"
+         >
+         <div
+            class=" w-4/5 md:w-1/3 h-48 md:h-72 rounded-3xl md:mr-12"
+            >
+            <img
+               src="/teamplan.png"
+               alt="Teamplanpng"
+               class="w-auto md:w-auto mt-10 mx-auto my-auto h-48 md:h-72 rounded-3xl md:mr-12"
+               />
+         </div>
+         <div class="text-center md:w-1/3 md:ml-36">
+            <h2 class="text-2xl md:text-3xl font-bold pb-6 md:pt-0">
+               Het Teamplan
+            </h2>
+            <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+               In het Teamplan legt het team de ambities voor de komende jaren vast, door haar doelen te bepalen. Aan deze doelen koppelt het team 'eigenaren'. Zij dragen zorg voor de realisatie van deze doelen.
+            </p>
+            <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+               Het teamplan wordt gevoed op basis van interne en externe analyses die we samen met het team uitvoeren. In workshops gaat het team zelf aan de slag en met direct resultaat. Het uitgangspunt is dat de teams zelf niets hoeven voor te bereiden en uit te werken. Een coach begeleidt het proces. De teamleden participeren actief en delen kennis en inzichten. Voor de workshops gebruiken we in praktijk ontwikkelde formats en spellen 
+            </p>
+         </div>
+      </div>
+   </section>
+   <section class="mb-16 relative">
+      <div class="relative w-full mx-auto">
+         <img 
+            src="/implementatie.jpg"
+            alt="Implementatieimg" 
+            class="w-full md:w-full h-[20rem] flex justify-center items-center md:h-80 object-cover rounded-b-3xl md:rounded-r-full blur-sm"
+            />
+         <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
+            >
+            <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
+               >
+               <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Implementatie</h2>
+               <p class="w-11/12 md:w-2/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
+                  Het Spiegelspel kan kleinschalig worden toegepast, bijvoorbeeld voor één team of een aantal pilot teams. Vanuit zo'n kleinschalige startopstelling kan het Spiegelconcept opgeschaald worden naar een organisatiebreed toepassing.
+               </p>
+            </div>
+         </div>
+      </div>
+   </section>
 </main>
-
 <Footer />
-
