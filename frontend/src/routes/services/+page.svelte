c56d76cdff2803dc4f3f025762b1e3493b12b380
Jayen <jayden.budike@gmail.com>
2024-11-20 10:02:59 +0100

    Testing features

--- frontend/src/routes/services/+page.svelte
+++ frontend/src/routes/services/+page.svelte
@@ -2,15 +2,17 @@
   import Header from "../../components/Header.svelte";
   import Footer from "../../components/Footer.svelte";
 
-  // Array of services with names and descriptions
+  // Array sdienstverlening dummy
   const services = [
     { name: "Teamkompas.", description: "Expert advice to optimize your team's strategy and performance." },
     { name: "Zelfevaluatie.", description: "Tailored plans to align your team's goals with organizational success." },
     { name: "Prestatiesturing.", description: "In-depth analysis of team dynamics and performance." },
     { name: "Teamplan en -evaluatie.", description: "Streamline your processes for better efficiency and results." },
     { name: "Omgevingsanalyse -en aanpak.", description: "Develop effective strategies to engage key stakeholders." },
-    { name: "Stakeholdersanalyse en -strategie.", description: "Tools and methods to track and enhance team performance." }
+    { name: "Stakeholdersanalyse en -strategie.", description: "Tools and methods to track and enhance team performance." },
     { name: "Processen.", description: "We bepalen de kritische processen en onderkende risico's dekken we af met maatregelen." },
+    { name: "Projectmanagement.", description: "Wij ondersteunen uw projecten met de juiste manieren en technieken." },
+    { name: "Projectmanagement.", description: "Wij ondersteunen uw projecten met de juiste manieren en technieken." }
   
 
   ];
@@ -25,12 +27,13 @@
       <div
         class="relative bg-white shadow-lg rounded-lg p-6 overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl"
       >
-        <div class="absolute inset-0 bg-gradient-to-br from-red-400 to-yellow-400 opacity-0 hover:opacity-50 transition-opacity"></div>
+        <div class="absolute inset-0 bg-gradient-to-br from-red-400 to-blue-400 opacity-0 hover:opacity-50 transition-opacity"></div>
         <h2 class="text-xl font-bold mb-2">{name}</h2>
         <p class="text-gray-600">{description}</p>
       </div>
     {/each}
   </div>
+  
 </main>
 
 <Footer />
