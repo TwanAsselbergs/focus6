a30487ca26d421b3ef708a568e0aa49468e28304
LucasDG1 <144344269+LucasDG1@users.noreply.github.com>
2024-11-21 13:10:35 +0100

    test commit

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -6,7 +6,8 @@ export const nodes = [
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
@@ -15,7 +16,8 @@ export const dictionary = {
 		"/": [2],
 		"/concept": [3],
 		"/contact": [4],
-		"/services": [5]
+		"/services": [5],
+		"/spiegelspel": [6]
 	};
 
 export const hooks = {
