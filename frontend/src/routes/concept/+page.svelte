5a8001c4552557790e7fb4d37b035cfdfb035ee0
zilla416 <iicyotb@gmail.com>
2024-11-21 15:12:27 +0100

    Conceptpagina af

--- frontend/src/routes/concept/+page.svelte
+++ frontend/src/routes/concept/+page.svelte
@@ -79,7 +79,7 @@
             <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
                >
                <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Aanpak</h2>
-               <p class="w-11/12 md:w-2/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
+               <p class="w-11/12 md:w-4/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 transition-all duration-500 ease-in-out hover:[text-shadow:_0_2px_4px_rgb(255_255_255)]">
                   Het Spiegelconcept bestaat uit verschillende instrumenten, die onderling samenhangen. Hieronder bespreken we kort de belangrijkste instrumenten. 
                </p>
             </div>
@@ -133,21 +133,157 @@
             </p>
          </div>
       </div>
+            <div class="flex flex-col md:flex-row justify-center items-center">
+         <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
+            <h2
+               class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
+               >
+               Spiegelspel, Klantarena, Stakeholders-  en Omgevingsanalyse
+            </h2>
+            <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
+Naast het Team Kompas en het Teamplan staan in het Spiegelconcept 4 workshops centraal. In deze workshops helpen we de teams begeleiden om zelf interne en externe analyses en evaluaties uit te voeren op hun werkterrein. Het Spiegelspel (1) is een zelfevaluatie (audit) waarbij het team haar eigen functioneren in de vorm een spel toetst.
+
+In een klantenonderzoek (2), zoals de klantarena, gaat het team de dialoog aan met klanten en stakeholders. In een stakeholdersanalyse (3) brengt het team haar stakeholders in beeld en bepaalt zij haar strategie. Met de omgevingsanalyse (4) analyseren we samen de belangrijkste ontwikkelingen en de impact op het werk. Deze analyses leveren elk een rapportage op met sterke- en verbeterpunten.  
+            </p>
+         </div>
+         <img
+            src="/stakeholders.png"
+            alt="Teamkompaspng"
+            class="rounded-3xl w-2/5 md:w-1/4 mt-10 md:mt-0 mb-5"
+            />
+      </div>
    </section>
    <section class="mb-16 relative">
-      <div class="relative w-full mx-auto">
+<div class="relative w-full mx-auto">
   <div
-    class="bg-[#1C2122] py-24 md:py-48 h-72 md:h-auto rounded-b-3xl md:rounded-r-full text-center"
+    id="black-div"
+    class="bg-[#1C2122] py-4 md:py-24 md:pt-1 rounded-b-3xl md:rounded-r-full text-center animate-fade-in transition-all duration-500"
   >
-         <div class="absolute inset-0 rounded-3xl text-white pl-4 pt-16 md:pt-24 md:pl-48"
-            >
-            <div class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
+    <div class="text-white px-4 pt-16 md:pt-24 md:px-48">
+      <div
+        class="w-full md:w-2/3 flex justify-center items-center md:items-start flex-col md:flex-none"
+      >
+        <h2
+          class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left"
+        >
+          Implementatie
+        </h2>
+        <p
+          id="text-content"
+          class="w-full sm:w-11/12 md:w-full text-center md:text-left text-lg md:text-base pt-4 pb-8 md:overflow-visible overflow-hidden transition-all duration-500 ease-in-out"
+          style="max-height: 6rem; transition: max-height 0.5s ease-in-out, padding 0.5s ease-in-out;"
+        >
+          Het Spiegelspel kan kleinschalig worden toegepast, bijvoorbeeld voor
+          één team of een aantal pilot teams. Vanuit zo'n kleinschalige
+          startopstelling kan het Spiegelconcept opgeschaald worden naar een
+          organisatiebreed toepassing. Zij kan zich hierin door ons laten
+          begeleiden of via een train-de-trainer constructie de implementatie
+          zelf ter hand nemen. Bij een organisatiebrede toepassing van het
+          SpiegelconceptIn doorloop je een vergelijkbare aanpak (als bij de
+          teams) maar dan op concernniveau. De speerpunten van het concern
+          geven dan richting aan de invulling van de teamplannen en de analyse
+          van de teamplannen levert input voor het concernplan, in de vorm van
+          'rode draden' voor verbetering en ontwikkeling.
+        </p>
+        <button
+          id="toggle-button"
+          class="text-white bg-gray-700 px-4 py-2 rounded-md mt-4 md:hidden hover:bg-gray-600"
+        >
+          Read More
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  const toggleButton = document.getElementById("toggle-button");
+  const textContent = document.getElementById("text-content");
+  const blackDiv = document.getElementById("black-div");
+
+  let expanded = false;
+
+  toggleButton.addEventListener("click", () => {
+    expanded = !expanded;
+    if (expanded) {
+      textContent.style.maxHeight = "1000px"; // Increase max height to show all content
+      textContent.style.paddingBottom = "2rem"; // Add padding for better spacing when expanded
+      toggleButton.innerText = "Read Less"; // Change button text
+    } else {
+      textContent.style.maxHeight = "4rem"; // Collapse the text
+      textContent.style.paddingBottom = "0"; // Remove padding when collapsed
+      toggleButton.innerText = "Read More"; // Reset button text
+    }
+  });
+</script>
+
+   <hr class="w-1/4 flex justify-center items-center mx-auto pb-12 md:pb-14" />
+                  <div class="flex flex-col md:flex-row justify-center items-center">
+         <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
+            <h2
+               class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
                >
-               <h2 class="text-4xl md:text-7xl font-semibold pb-4 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)] text-center md:text-left">Implementatie</h2>
-               <p class="w-11/12 md:w-2/4 text-center md:text-left text-lg md:text-base pt-4 pb-8 [text-shadow:_0_2px_4px_rgb(0_0_0_/_1)]">
-                  Het Spiegelspel kan kleinschalig worden toegepast, bijvoorbeeld voor één team of een aantal pilot teams. Vanuit zo'n kleinschalige startopstelling kan het Spiegelconcept opgeschaald worden naar een organisatiebreed toepassing.
-               </p>
-            </div>
+               Analyse en evaluatie in 3 jaar cyclus
+            </h2>
+            <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
+Jaarlijks maakt het team een nieuw teamplan. De zelfevaluatie en het klantenonderzoek komen jaarlijks terug. De andere analyse & evaluatie instrumenten worden gespreid over 3 jaar uitgevoerd. Eventueel kunnen alternatieve audits of evaluaties worden ingepland. Hiermee kan aangesloten worden op de 3 jarige certificeringscyclus.
+            </p>
+         </div>
+         <img
+            src="/evaluatie.png"
+            alt="Teamkompaspng"
+            class="rounded-3xl w-2/5 md:w-1/4 mt-10 md:mt-0 mb-5"
+            />
+      </div>
+
+                        <div class="flex flex-col md:flex-row justify-center items-center">
+         <div class="md:w-1/2 flex justify-center items-center flex-col md:block">
+            <h2
+               class="text-center md:text-left text-2xl md:text-3xl font-bold pb-6"
+               >
+               Teamplan cyclus per jaar
+            </h2>
+            <p class="w-10/12 md:w-full text-center md:text-left md:pr-48 text-lg">
+Op teamniveau geeft het team invulling aan 'Samen sturen' middels een wekelijkse 'check-in', een maandelijkse 'check-out' en een 'refresh' per kwartaal. In een weekstart vindt de check-in plaats: het team komt in actie. Bij de check-up kijkt het team hoe de kritische processen lopen en of bijsturing nodig is. Bij de Refresh kijkt het team opnieuw naar het teamplan en bepaalt het team of zij nog de 'goede dingen' doet.
+            </p>
+         </div>
+         <img
+            src="/proces.png"
+            alt="Teamkompaspng"
+            class="rounded-3xl w-2/5 md:w-1/4 mt-10 md:mt-0 mb-5"
+            />
+      </div>
+            <div
+         class="flex flex-col-reverse md:flex-row items-center md:justify-center my-24 md:my-48"
+         >
+         <div
+            class=" w-4/5 md:w-1/3 h-48 md:h-72 rounded-3xl md:mr-12"
+            >
+            <img
+               src="/logo.png"
+               alt="Teamplanpng"
+               class="w-auto md:w-auto mt-10 mx-auto my-auto h-48 md:h-72 rounded-3xl md:mr-12"
+               />
+         </div>
+         <div class="text-center md:w-1/3 md:ml-36">
+            <h2 class="text-2xl md:text-3xl font-bold pb-6 md:pt-0">
+               Borging en certificering
+            </h2>
+            <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+Indien uw organisatie aan de slag gaat met het Spiegelconcept, kunt u, als u dat wenst, ook voldoen aan de certificeringseisen van de ISO 9001 (of HKZ). 
+            </p><br>
+            <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+In de aanpak zijn vele normvereisten geborgd, zoals leiderschap, interne audits en PDCA. Als u reeds een certificaat heeft kan dit naadloos ingepast worden.
+            </p><br>
+                        <p
+               class="w-10/12 flex justify center items-center mx-auto flex-col text-lg"
+               >
+Mocht u nog niet voldoen aan de gestelde vereisten dan kunt u met de Spiegelconcept aanpak gaan voldoen aan de vereisten en uw certificaat behalen.
+            </p>
          </div>
       </div>
    </section>
